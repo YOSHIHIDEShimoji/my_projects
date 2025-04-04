84ff7d709a05eb6d87baf2d6c5f22af449ad53dd
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-04-04 13:33:27 +0900

    26

--- my_workspace/src/supu_practice.py
+++ my_workspace/src/supu_practice.py
@@ -1,4 +1,6 @@
 odd_numbers = [1, 3, 5, 7]
 even_numbers = [2, 4, 6, 8]
-numbers = odd_numbers + even_numbers
-print(numbers)
+odd_numbers.append(9)
+even_numbers.remove(8)
+print(odd_numbers)
+print(even_numbers)
