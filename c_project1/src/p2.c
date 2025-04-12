fd74b9e49658630b4d6150b94a2a8aa028496c4b
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-04-12 20:49:25 +0900

    change c project

---
+++ c_project1/src/p2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int x; /*integer*/
+	for(x=0; x<=7; x=x+1){
+		printf("Hello, world!\n");
+	}
+	return 0;
+}
---
+++ c_project1/src/p3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int x;
+	for(x=1; x<=7; x++){
+		printf("6*%d=%d\n", x, 6*x);
+	}
+	return 0;
+}
